fd02204644d301bb48267165c7a4160078cb6174
majeed-abdul <majeedabdul.941485@gmail.com>
2023-11-03 20:21:32 +0500

    publishing all done and fetching testing done

--- lib/network/config.dart
+++ lib/network/config.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:web3dart/web3dart.dart';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart';
+import 'package:hex/hex.dart';
 
 var myAddress =
-    "0xb784dfdd34b87e31ee40025413e6d1798fa200c304f25c0ae4b64a6930664fa3";
+    '9b1973a1ea2916317aed13b9575d8461125341956048c458bdcf78f9b04735f2';
 
-Client httpClient = Client();
-Web3Client ethClient = Web3Client(
-  "https://sepolia.infura.io/v3/e13262dbb2d84336b7b999c45469350c",
+final Client httpClient = Client();
+final Web3Client ethClient = Web3Client(
+  "https://sepolia.infura.io/v3/8fb9776787ea4283a449ca99699b5cad",
   httpClient,
 );
 
@@ -20,8 +21,6 @@ Future<List<dynamic>> query(String functionName, List<dynamic> args) async {
     function: ethFunction,
     params: args,
   );
-  debugPrint('\narg ${args.toString()}');
-  debugPrint('res ${result.toString()}');
   return result;
 }
 
@@ -37,19 +36,73 @@ Future<String> submit(String functionName, List<dynamic> args) async {
       function: ethFunction,
       parameters: args,
     ),
-    // fetchChainIdFromNetworkId: true,
+    chainId: null,
+    fetchChainIdFromNetworkId: true,
   );
-  debugPrint('\narg ${args.toString()}');
-  debugPrint('txHAsh ${result.toString()}');
+  debugPrint(' ===== Tx-Hash: $result');
   return result;
 }
 
 Future<DeployedContract> loadContract() async {
-  String abi = await rootBundle.loadString("assets/abi.json");
-  String contractAddress = "0x4969b7F1e33D8e8BC4235f2233c0632d63eDe29d";
+  String abi = await rootBundle.loadString('assets/abi.json');
+  String contractAddress = '0x8a2B53599c98333989F8b9e8bF88DA4971e12C5D';
   final contract = DeployedContract(
-    ContractAbi.fromJson(abi, "Ins"),
+    ContractAbi.fromJson(abi, 'Institution'),
     EthereumAddress.fromHex(contractAddress),
   );
   return contract;
 }
+
+Future<void> getEventsFromTxHash(String txHash) async {
+  final transaction = await ethClient.getTransactionByHash(txHash);
+  if (transaction == null) {
+    debugPrint('transaction not found');
+    return;
+  }
+  DeployedContract contract = await loadContract();
+  final event = contract.event('push');
+  final filter = FilterOptions.events(
+    event: event,
+    contract: contract,
+    fromBlock: BlockNum.exact(transaction.blockNumber.blockNum),
+    toBlock: BlockNum.exact(transaction.blockNumber.blockNum),
+  );
+  final events = await ethClient.getLogs(filter);
+
+  for (var event in events) {
+    debugPrint(' ===== Event: ${event.toString}');
+
+    var hexData = '${event.data}';
+    if (hexData.startsWith('0x')) {
+      hexData = hexData.substring(2);
+    }
+    List<int> bytes = HEX.decode(hexData);
+    String decodedString = String.fromCharCodes(bytes);
+    String signature = decodedString.substring(96, 96 + 142);
+    String dataHash = decodedString.substring(288, 288 + 64);
+
+    debugPrint(' = EventTopic = ${event.topics}');
+    debugPrint(' ===== Signature: $signature');
+    debugPrint(' ===== Data Hash: $dataHash');
+  }
+}
+
+// Future<void> getEventLogs() async {
+//   final contractAddress = 'CONTRACT_ADDRESS';
+//   final contract = DeployedContract(
+//     ContractAbi.fromJson(ABI, 'ContractName'),
+//     EthereumAddress.fromHex(contractAddress),
+//   );
+
+//   final filter = FilterOptions.events(
+//     contract: contract,
+//     event: 'EventName', // Name of the event you want to filter
+//     fromBlock: BlockNum.exact(1),
+//     toBlock: BlockNum.pending,
+//   );
+
+//   final events = await web3.getLogs(filter);
+//   for (var event in events) {
+//     print('Event: $event');
+//   }
+// }
---
+++ lib/screens/code_display.dart
@@ -0,0 +1,66 @@
+import 'package:barcode_widget/barcode_widget.dart';
+import 'package:flutter/material.dart';
+
+class CodeDisplayScreen extends StatefulWidget {
+  const CodeDisplayScreen({super.key, this.data});
+  final String? data;
+
+  @override
+  State<CodeDisplayScreen> createState() => _CodeDisplayScreenState();
+}
+
+class _CodeDisplayScreenState extends State<CodeDisplayScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        centerTitle: true,
+        title: const Text('QR Code'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(children: [
+          displayOutputCode(context),
+          const SizedBox(height: 15),
+          const Text('Past QR Code to back of degree'),
+          const Spacer(),
+          const Text(
+            'This QR Code contains data hash(sha256) and txHash',
+            style: TextStyle(fontSize: 11),
+          ),
+        ]),
+      ),
+    );
+  }
+
+//
+
+//
+
+//                  E X T R A S
+
+//
+
+//
+
+  BarcodeWidget displayOutputCode(BuildContext context) {
+    return BarcodeWidget(
+      height: MediaQuery.of(context).size.width <=
+              MediaQuery.of(context).size.height
+          ? MediaQuery.of(context).size.width - 60 * 2
+          : MediaQuery.of(context).size.height - 60 * 2,
+      data: widget.data ?? '',
+      barcode: Barcode.qrCode(),
+      padding: const EdgeInsets.all(20),
+      errorBuilder: (context, error) => _onError(error),
+    );
+  }
+
+  Widget _onError(String message) {
+    return Text(
+      message.substring(message.indexOf('Barcode, '), message.length - 1),
+    );
+  }
+}
